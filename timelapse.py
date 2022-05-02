3e39e9b9b34c95e57b46729731f83f16c1fc641a
Theo Themelis <themelis@sch.gr>
2022-05-02 10:43:10 +0300

    Προσθήκη αρχείο ρυθμίσεων

--- timelapse.py
+++ timelapse.py
@@ -3,34 +3,58 @@ import os
 import sys
 from datetime import datetime
 from time import sleep
+import yaml
 
-def createDir(dirName): # Συνάρτηση δημιουργία φακέλου
+camera = PiCamera()  # Δημιουργία αντικειμένου κάμερας
+config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))  # Άνοιγμα του αρχείο ρυθμίσεων
+
+def createDir(dirName):  # Συνάρτηση δημιουργία φακέλου
     try:
         os.makedirs(dirName)
-        print("Δημιουργήθηκε ο φάκελος " , dirName)
+        print("Δημιουργήθηκε ο φάκελος ", dirName)
     except FileExistsError:
-        print("Ο φάκελος " , dirName ,  " υπάρχει ήδη!!")
+        print("Ο φάκελος ", dirName,  " υπάρχει ήδη!!")
 
-minutesRun = 240 # Χρόνος κατά τον οποίο θα γίνεται λήψη φωτογραφιών (λεπτά)
-photoInterval = 15 # Χρόνος μεταξύ δύο λήψεων (δευτερόλεπτα)
-fps = 30 # Πλαίσια ανά δευτερόλεπτο
-numberPhotos = int((minutesRun*60)/photoInterval) # Πλήθος φωτογραφιών
+if config['resolution']:
+    camera.resolution = (
+    config['resolution']['width'],
+    config['resolution']['height']
+)
+
+if config['iso']:
+    camera.iso = config['iso']
+
+if config['shutter_speed']:
+    camera.shutter_speed = config['shutter_speed']
+    sleep(1)
+    camera.exposure_mode = 'off'
+
+if config['white_balance']:
+    camera.awb_mode = 'off'
+    camera.awb_gains = (
+        config['white_balance']['red_gain'],
+        config['white_balance']['blue_gain']
+    )
+
+if config['rotation']:
+    camera.rotation = config['rotation']
+
+numberPhotos = int((config['minutesRun']*60)/config['photoInterval'])  # Πλήθος φωτογραφιών
 print("Θα ληφθούν ", numberPhotos, " φωτογραφίες")
 
-camera = PiCamera() # Δημιουργία αντικειμένου κάμερας
-#camera.resolution = (1024, 768) # Ανάλυση φωτογραφιών
-#camera.iso = 0 # Ρύθμιση του iso της κάμερας
-#camera.shutter_speed = 0 # Ρύθμιση του χρόνο του κλείστρου της κάμερας
-#camera.white_balance = 
-
-dirName = os.path.join(sys.path[0],'/media/usb/' +'photos-' + datetime.now().strftime('%d%m%y-%H%M%S')) # Όνομα φακέλου που θα αποθηκευτούν οι φωτογραφίες
+dirName = os.path.join(sys.path[0], str(config['dir_path']) + 'photos-' + datetime.now().strftime('%d%m%y-%H%M%S'))  # Όνομα φακέλου που θα αποθηκευτούν οι φωτογραφίες
 
 createDir(dirName)  # Κλήση της συνάρτησης δημιουργίας φακέλου
 
 for i in range(numberPhotos):   # Επαναληπτική διαδικασία λήψης φωτογραφιών
-    camera.capture(dirName + '/image{0:08d}.jpg'.format(i)) # Λήψη φωτογραφίας
-    sleep(photoInterval)    # Χρόνος αναμονής μεταξύ δύο λήψεων
+    camera.capture(dirName + '/image{0:08d}.jpg'.format(i))  # Λήψη φωτογραφίας
+    sleep(config['photoInterval'])    # Χρόνος αναμονής μεταξύ δύο λήψεων
 print("Ολοκληρώθηκε η λήψη των φωτογραφιών")
-print("Ακολουθεί η δημιουργία του βίντεο. Η διαδικασία θα κρατήσει αρκετή ώρα!!")
 
-os.system('ffmpeg -i ' + dirName + '/image%08d.jpg -c:v libx264 ' + dirName + '/timelapse.mp4') # Δημιουργία του βίντεο
+if config['create_gif']:   # Δημιουργία animated Gif
+    print ('\nΔημιουργία animated gif.\n')
+    os.system('convert -delay 10 -loop 0 ' + dirName + '/image*.jpg ' + dirName + '/animated.gif')
+
+if config['create_video']:
+    print("\nΔημιουργία βίντεο. Η διαδικασία θα κρατήσει αρκετή ώρα!!\n")
+    os.system('ffmpeg -i ' + dirName + '/image%08d.jpg -c:v libx264 ' + dirName + '/video.mp4')  # Δημιουργία του βίντεο
